209277241a8f426453acba82a074d5ecb85858c1
xupea <xupea@xupeadeMacBook-Pro.local>
2024-03-21 12:01:55 +0800

    feat: make sure it only work under csdn blog website

--- src/background/index.ts
+++ src/background/index.ts
@@ -3,3 +3,24 @@ console.log('background is running')
 chrome.runtime.onMessage.addListener((request) => {
   chrome.tabs.reload(request.id)
 })
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete') {
+    let url = new URL(tab.url!)
+    let domain = url.hostname
+
+    if (domain === 'blog.csdn.net') {
+      // 启用弹出页
+      chrome.action.setPopup({
+        tabId: tabId,
+        popup: 'popup.html', // 指定你的弹出页面
+      })
+    } else {
+      // 禁用弹出页，可以设置为空字符串或不设置popup属性
+      chrome.action.setPopup({
+        tabId: tabId,
+        popup: '', // 禁用弹出页面
+      })
+    }
+  }
+})
